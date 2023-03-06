ea4f2d5a94203de21cae8c93ae5277f9801ba4f5
simonn70 <simonadjei70@gmail.com>
2023-03-06 14:24:26 +0000

    create and update files

---
+++ server/constants/product.js
@@ -0,0 +1,128 @@
+export const products = [
+    { 
+        id: 'product1',
+        url: 'https://rukminim1.flixcart.com/image/150/150/kapoo7k0/electric-kettle/p/6/s/pigeon-favourite-original-imafs7xhj5uwgrh4.jpeg?q=70', 
+        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kapoo7k0/electric-kettle/p/6/s/pigeon-favourite-original-imafs7xhj5uwgrh4.jpeg?q=70',
+        title: {
+            shortTitle: 'product',
+            longTitle: 'this is a product'
+        }, 
+        price: {
+            mrp: 1195,
+            cost: 625,
+            discount: '47%'
+        },
+        quantity: 1,
+        description: 'buy now',
+        discount: 'Extra 10% Off', 
+        tagline: 'Deal of the day' 
+    },
+    { 
+        id: 'product2',
+        url: 'https://th.bing.com/th/id/OIP.2udnyX-diTB3AWV8RWRhFQHaEW?w=306&h=180&c=7&r=0&o=5&pid=1.7',
+        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kl6wx3k0/sandwich-maker/8/r/d/sandwich-01-flipkart-smartbuy-original-imagydds4zthxt8z.jpeg?q=70',
+        title: {
+            shortTitle: 'product',
+            longTitle: 'this is a new product'
+        },
+        price: {
+            mrp: 1499,
+            cost: 899,
+            discount: '40%'
+        },
+        quantity: 1,
+        description: 'this a product',
+        discount: 'From 99+5% Off', 
+        tagline: 'Pestige, Nova & more' 
+    },
+    { 
+        id: 'product3',
+        url: 'https://th.bing.com/th/id/OIP.2udnyX-diTB3AWV8RWRhFQHaEW?w=306&h=180&c=7&r=0&o=5&pid=1.7', 
+        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kohigsw0/resistance-tube/c/s/e/new-adjustable-single-resistance-tube-multicolor-na-ajro-deal-original-imag2xg88mhmwxz5.jpeg?q=70', 
+        title: {
+            shortTitle: 'product',
+            longTitle: 'AJRO DEAL New Adjustable Single Resistance Tube (Multicolor) Resistance Tube  (Multicolor)'
+        }, 
+        price: {
+            mrp: 499,
+            cost: 166,
+            discount: '66%'
+        },
+        quantity: 1,
+        description: 'This unique product ',
+        discount: 'Upto 70% Off', 
+        tagline: 'Deal of the Day' 
+    },
+    { 
+        id: 'product4',
+        url: 'https://th.bing.com/th/id/OIP.2udnyX-diTB3AWV8RWRhFQHaEW?w=306&h=180&c=7&r=0&o=5&pid=1.7', 
+        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kll7bm80/smartwatch/c/1/n/43-mo-sw-sense-500-android-ios-molife-original-imagyzyycnpujyjh.jpeg?q=70',
+        title: {
+            shortTitle: 'Smart Watches',
+            longTitle: 'Molife Sense 500 Smartwatch  (Black Strap, Freesize)',
+        }, 
+        price: {
+            mrp: 6999,
+            cost: 4049,
+            discount: '42%'
+        },
+        quantity: 1,
+        description: 'The Molife Sense 500, a brilliant smartwatch with a beautiful large display. Say hello to the infinity 1.7-inch display with 2.5D curved edges. Thanks to seamless Bluetooth 5.0 connectivity, you wont have to keep waiting. Bring a change to your outfit every day with changeable straps. A splash of color every day keeps the boredom away.',
+        discount: 'Grab Now', 
+        tagline: 'Best Seller' 
+    },
+    { 
+        id: 'product5',
+        url:'https://th.bing.com/th/id/OIP.RFnEKabBollap8uJieMHGAHaE8?w=303&h=202&c=7&r=0&o=5&pid=1.7',
+        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/k3uhhu80/hair-dryer/n/m/t/nova-2800-w-professional-nhp-8220-original-imafmvwfhmzsxdrw.jpeg?q=70', 
+        title: {
+            shortTitle: 'product',
+            longTitle: 'nothing'
+        }, 
+        price: {
+            mrp: 1899,
+            cost: 1124,
+            discount: '40%'
+        },
+        quantity: 1,
+        description: '',
+        discount: 'From GHS499', 
+        tagline: 'Kubra, Nova & more' 
+    },
+    { 
+        id: 'product6',
+        url: 'https://th.bing.com/th/id/OIP.RFnEKabBollap8uJieMHGAHaE8?w=303&h=202&c=7&r=0&o=5&pid=1.7',
+        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kk01pjk0/fan/d/d/l/tiktik-quiet-portable-table-fan-zigma-original-imafzg7ftzuckpad.jpeg?q=70', 
+        title: {
+            shortTitle: 'Table Fans',
+            longTitle: 'Portable 300 mm Ultra High Speed 3 Blade Table Fan  (Black, Pack of 1)'
+        }, 
+        price: {
+            mrp: 2250,
+            cost: 1199,
+            discount: '46%'
+        },
+        quantity: 1,
+        description: 'Table Fan. Perfect size fan for use on a table, desk or in an RV. Whisper quiet, powerful airflow and reliable operation in a compact 6" size. Two adjustable speeds to customize airflow: high or low settings. Tough break-resistant ABS plastic blades. ',
+        discount: 'Minimum 40% Off', 
+        tagline: 'Top Selling' 
+    },
+    { 
+        id: 'product7',
+        url: 'https://th.bing.com/th/id/OIP.2udnyX-diTB3AWV8RWRhFQHaEW?w=306&h=180&c=7&r=0&o=5&pid=1.7',
+        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kcgk1ow0/headphone/n/u/a/235v2-fast-charging-boat-original-imaftk6us4af7bca.jpeg?q=70', 
+        title: {
+            shortTitle: 'Headphones',
+            longTitle: 'boAt Rockerz 235v2 with ASAP charging Version 5.0 Bluetooth Headset '
+        }, 
+        price: {
+            mrp: 2990,
+            cost: 1199,
+            discount: '59%'
+        },
+        quantity: 1,
+        description: 'Let music brighten up your mood anytime, anywhere with the boAt 235v2 Fast Charging Bluetooth Headset. This Bluetooth headset features a Call Vibration Alert, a Fast Charging Technology, and Easy Access Controls to listen to and manage your favorite music with ease.',
+        discount: 'Minimum 50% Off', 
+        tagline: 'Grab Now!' 
+    }
+];
---
+++ server/database/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const Connection = async () => {
+    // const URL = `mongodb://${username}:${password}@ecommerceweb-shard-00-00.fdvft.mongodb.net:27017,ecommerceweb-shard-00-01.fdvft.mongodb.net:27017,ecommerceweb-shard-00-02.fdvft.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-8a6bhp-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    // const URL = `mongodb://${username}:${password}@ecommerce-shard-00-00.fdvft.mongodb.net:27017,ecommerce-shard-00-01.fdvft.mongodb.net:27017,ecommerce-shard-00-02.fdvft.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-ilaj5d-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    const URL = `mongodb+srv://simonadjei70:admin@cluster0.al2g0wo.mongodb.net/?retryWrites=true&w=majority`
+    try {
+        await mongoose.connect(URL,{ useNewUrlParser: true });
+        console.log('Database Connected Succesfully');
+    } catch(error) {
+        console.log('Error: ', error.message);
+    }
+
+};
+
+export default Connection;
